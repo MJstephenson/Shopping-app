4793b405dbef69b38ee43415bb57150d4976c870
MJstephenson <cyclomark@gmail.com>
2023-09-09 12:30:32 +0000

    Add products app

---
+++ products/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Category, AlcoholType, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name',)
+    ordering = ('name',)
+
+class AlcoholTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'sku', 'category', 'alcohol_type', 'price', 'rating', 
+        'alcohol_content', 'volume', 'country_of_origin', 'year_of_production', 
+        'stock'
+    )
+    ordering = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(AlcoholType, AlcoholTypeAdmin)
+admin.site.register(Product, ProductAdmin)

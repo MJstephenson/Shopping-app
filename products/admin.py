7068b4e6fe5bbade429cff4e2a8116392b25cce0
MJstephenson <cyclomark@gmail.com>
2023-09-10 15:31:08 +0000

    Add deals, clearance and new arrival to model

--- products/admin.py
+++ products/admin.py
@@ -1,21 +1,28 @@
 from django.contrib import admin
 from .models import Category, AlcoholType, Product
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'friendly_name',)
     ordering = ('name',)
+
 
 class AlcoholTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     ordering = ('name',)
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name', 'sku', 'category', 'alcohol_type', 'price', 'rating', 
         'alcohol_content', 'volume', 'country_of_origin', 'year_of_production', 
-        'stock', 'image',
+        'stock', 'image', 'new_arrival', 'deal', 'clearance'
     )
+
+    list_filter = ('new_arrival', 'deal', 'clearance')
+
     ordering = ('name',)
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(AlcoholType, AlcoholTypeAdmin)
--- products/models.py
+++ products/models.py
@@ -34,6 +34,9 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    new_arrival = models.BooleanField(default=False, help_text="Check this if the product is a new arrival.")
+    deal = models.BooleanField(default=False, help_text="Check this if the product is on deal.")
+    clearance = models.BooleanField(default=False, help_text="Check this if the product is on clearance.")
     rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
     alcohol_content = models.DecimalField(max_digits=4, decimal_places=1, help_text="Alcohol content in percentage")
     volume = models.DecimalField(max_digits=5, decimal_places=0, help_text="Volume in ml")
@@ -43,6 +46,7 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=0)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name

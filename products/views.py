9867861ba70e71f1830728116f5963dd244b1ef6
MJstephenson <cyclomark@gmail.com>
2024-01-10 15:10:19 +0000

    Add product view, url & template for product  admin

--- products/views.py
+++ products/views.py
@@ -1,62 +1,52 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
+# Create your views here.
 
-def search_products(query):
-    """ Utility function to filter products based on a query """
-    return Product.objects.filter(
-        Q(name__icontains=query) |
-        Q(description__icontains=query) |
-        Q(category__name__icontains=query)
-    )
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
-
-def fetch_special_products(special):
-    """ Utility function to fetch products based on special offers """
-    filters = {
-        'deal': Q(deal=True),
-        'new_arrival': Q(new_arrival=True),
-        'clearance': Q(clearance=True),
-        'all_specials': Q(deal=True) | Q(new_arrival=True) | Q(clearance=True),
-    }
-    return Product.objects.filter(filters.get(special, Q()))
-
-
-def products(request):
-    """ A view to show all products, including specials, sorting, and search queries """
-
-    query = request.GET.get('q')
-    category = request.GET.get('category')
-    special = request.GET.get('special')
-    sort = request.GET.get('sort')
-    direction = request.GET.get('direction')
-    current_sorting = f"{sort}_{direction}" if sort and direction else 'None_None'
-
-    if query:
-        products = search_products(query)
-        if not products.exists():
-            messages.info(request, "No products found for your search.")
-    elif special:
-        products = fetch_special_products(special)
-    elif category:
-        products = Product.objects.filter(category__name__iexact=category)
-    else:
-        products = Product.objects.all()
-
-    # If categories are specified for filtering
+    products = Product.objects.all()
+    query = None
     categories = None
-    if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        products = products.filter(category__name__in=categories)
-        categories = Category.objects.filter(name__in=categories)
-    
-    # Sorting
-    if sort:
-        if direction == 'desc':
-            sort = f"-{sort}"
-        products = products.order_by(sort)
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
@@ -72,4 +62,20 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'products/product_detail.html', {'product': product})
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)

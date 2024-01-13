24685304ee00bc12fd4fef290cd5d886a6d882db
MJstephenson <cyclomark@gmail.com>
2024-01-13 17:17:14 +0000

    Create forms

---
+++ contact/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Contact
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))

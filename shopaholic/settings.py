d813d6789013001ea10bd2742dbe4ea0b0619794
MJstephenson <cyclomark@gmail.com>
2024-01-13 14:28:07 +0000

    Fix crispy forms and setup local dev environment

--- shopaholic/settings.py
+++ shopaholic/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'f9guw$!hlmujclg#$*)3f_01*z8x2-v%ej!kyy-hpg!ymn8w79')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['shopaholic-app.herokuapp.com', 'localhost', 'shopaholic-app-7001a5641b3f.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mjstephenso-shoppingapp-qorv8husujr.ws-eu107.gitpod.io']
 
 
 # Application definition
@@ -69,7 +70,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'shopaholic.urls'
 
-CRISPT_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {

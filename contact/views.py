cc47ca677a9923949b0c09473d189f511047096a
MJstephenson <cyclomark@gmail.com>
2024-01-26 12:20:40 +0000

    Add content to database

--- contact/views.py
+++ contact/views.py
@@ -5,11 +5,23 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            if request.user.is_authenticated:
+                user = request.user
+                form.instance.email = user.email
             form.save()
             return redirect('thank_you')
     else:
-        form = ContactForm()
-    return render(request, 'contact/contact.html', {'form': form})
+        if request.user.is_authenticated:
+            user = request.user
+            initial_data = {
+                'email': user.email,
+            }
+            form = ContactForm(initial=initial_data)
+        else:
+            form = ContactForm()
+
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
 
 def thank_you_view(request):
     return render(request, 'contact/thank_you.html')

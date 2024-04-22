8c27de48fe0006ab4728468562f5c70949e7e421
MJstephenson <cyclomark@gmail.com>
2024-04-22 18:14:30 +0000

    Add views for form submission

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SubscriberForm
 
-# Create your views here.
+def newsletter_signup(request):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('newsletter_confirmation')
+    else:
+        form = SubscriberForm()
+    return render(request, 'index.html', {'form': form})
+
+def newsletter_confirmation(request):
+    return render(request, 'newsletter_confirmation.html')

d1c570bce56a22ac8f80f321ed4d040d7db61727
MJstephenson <cyclomark@gmail.com>
2024-04-22 19:33:35 +0000

    Handle form validation for duplicate emails for user

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.contrib import messages
 from .forms import SubscriberForm
 
 def newsletter_signup(request):
     if request.method == 'POST':
         form = SubscriberForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('newsletter_confirmation')
+            try:
+                form.save()
+                return redirect('newsletter_confirmation')
+            except IntegrityError:
+                messages.error(request, "This email address has already been subscribed.")
+        else:
+            messages.error(request, "Please check your input.")
     else:
         form = SubscriberForm()
     return render(request, 'home/index.html', {'form': form})

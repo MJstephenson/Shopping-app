943613a7f977c78254a48bc49c091cd9a1c85ee0
MJstephenson <cyclomark@gmail.com>
2024-04-22 19:48:02 +0000

    Add newsletter CRUD to admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subscriber
 
-# Register your models here.
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'id')
+    search_fields = ('email',)
+    list_filter = ('email',)   
+
+admin.site.register(Subscriber, SubscriberAdmin)
+

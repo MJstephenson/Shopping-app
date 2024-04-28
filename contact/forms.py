cca6a0c3acc7e94f829b6cc8e8feb43e5639f3f1
MJstephenson <cyclomark@gmail.com>
2024-04-28 17:01:57 +0000

    Change forms.py code to fix html validation

--- contact/forms.py
+++ contact/forms.py
@@ -1,31 +1,29 @@
 from django import forms
 from .models import Contact
-from .models import SUBJECT_CHOICES
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 
-class ContactForm(forms.ModelForm):
-    SUBJECT_CHOICES = [
-        ('product_inquiry', 'Product Inquiry'),
-        ('order_status', 'Order Status'),
-        ('return_exchange', 'Return/Exchange'),
-        ('payment_issue', 'Payment Issue'),
-        ('technical_support', 'Technical Support'),
-        ('feedback', 'Feedback/Suggestions'),
-        ('other', 'Other'),
-    ]
-
-
-subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
-
+SUBJECT_CHOICES = [
+    ('product_inquiry', 'Product Inquiry'),
+    ('order_status', 'Order Status'),
+    ('return_exchange', 'Return/Exchange'),
+    ('payment_issue', 'Payment Issue'),
+    ('technical_support', 'Technical Support'),
+    ('feedback', 'Feedback/Suggestions'),
+    ('other', 'Other'),
+]
 
 class ContactForm(forms.ModelForm):
+  
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
+
     class Meta:
         model = Contact
-        fields = ['first_name', 'surname', 'subject', 'email', 'message']
+        fields = ['first_name', 'surname', 'subject', 'email', 'message']  
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(ContactForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.form_method = 'post'  
+        self.helper.add_input(Submit('submit', 'Submit'))  
